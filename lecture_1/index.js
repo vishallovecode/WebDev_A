c6a05a9cc9fedecb2a936af0d177af14bcfb8144
Vishal Sharma <785vishal443@gmail.com>
2025-01-07 23:42:13 +0530

    Lecture _1

---
+++ lecture_1/index.js
@@ -0,0 +1,56 @@
+// Logging the things  or Printing we use console.log
+
+
+console.log('I am Javascript Dont worry you will love me ..')
+
+
+
+ var employeeName = "Ram";
+ var age = 26;
+ var compnayName = "XYZ";
+
+//  console.log(employeeName)
+//  console.log(age)
+//  console.log(compnayName)
+
+console.log('Employee:', employeeName , "Age:", age , "Company: ",compnayName)
+
+
+// Variable declaration using let
+
+let  studentName= "Vishal"
+let collegeName= "NST";
+
+// Variable delcration using const
+const rollNo= 234567;
+
+
+// Note:1 Variable created using var can be re-declare and re-assign
+
+
+
+
+var aa = 300;
+var aa = 340; //  here aa is re-declare
+aa = 500; // re-assign 
+
+console.log(aa)
+
+
+
+// Note:2 Variable created using let cannot be re-declare and can re-assign
+
+let cityName = "Kanpur";
+ //var cityName; // you cannot re-declare let variable
+ cityName = "Sonipat"
+ console.log(cityName)
+
+ // Note:3 Variable created using const cannot be re-declare and cannot be re-assign
+ // Note: 4 You cannot just declare the const variable you need to assign the value as well
+ 
+//  const registrationNo; // this will throw the error
+const registrationNo = 3000023;
+
+registrationNo = 45667;  // re-assign the value
+
+
